4eef7397be48d7f22b12897f1b9c4eeaec82c789
Krivcova-VA <112896058+Krivcova-VA@users.noreply.github.com>
2024-11-30 13:22:38 +0300

    Delete number_7 directory

--- number_7/GraphicObjectBuilder.java
+++
@@ -1,9 +0,0 @@
-package number_7;
-
-// Интерфейс строителя графических объектов
-public interface GraphicObjectBuilder {
-    void setPosition(int x, int y);
-    void setColor(String color);
-    GraphicObject build();
-}
-
