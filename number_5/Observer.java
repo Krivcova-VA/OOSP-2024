07096327385ae1f920d6198203a44e876f1b1b7f
Krivcova-VA <112896058+Krivcova-VA@users.noreply.github.com>
2024-11-30 13:22:20 +0300

    Delete number_5 directory

--- number_5/Observer.java
+++
@@ -1,7 +0,0 @@
-package number_5;
-
-// Интерфейс наблюдателя
-public interface Observer {
-    void update(String product, double price);
-}
-
--- number_5/Subject.java
+++
@@ -1,9 +0,0 @@
-package number_5;
-
-// Интерфейс субъекта
-public interface Subject {
-    void registerObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-}
-
