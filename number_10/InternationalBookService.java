316589fb43b9d7a41ca8b4ec061070b63c41806f
Krivcova-VA <112896058+Krivcova-VA@users.noreply.github.com>
2024-11-30 13:20:52 +0300

    Delete number_10 directory

--- number_10/InternationalBookService.java
+++
@@ -1,9 +0,0 @@
-package number_10;
-
- // Подсистема для проверки международных книг
-public class InternationalBookService {
-    public boolean checkInternationalBook(String isbn) {
-        // Имитация проверки международной книги
-        return isbn.startsWith("978") || isbn.startsWith("979");
-    }
-}
--- number_10/OrderSystem.java
+++
@@ -1,8 +0,0 @@
-package number_10;
-
- // Подсистема для оформления заказов
-public class OrderSystem {
-    public void placeOrder(String title) {
-        System.out.println("Заказ на книгу \"" + title + "\" успешно оформлен.");
-    }
-}
